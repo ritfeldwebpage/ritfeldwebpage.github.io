5ba101ee5f9f85fe186f06ba813f3924c82e8c88
willemjritfeld <rit16005@byui.edu>
2019-10-23 17:55:22 -0400

    update

--- lesson4/js/lastmodified.js
+++ lesson4/js/lastmodified.js
@@ -1,1 +1,4 @@
-document.getElementById("lastMod").innerHTML = ( "Last Updated" + document.lastModified);
+var d = new Date();
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var month = ["January","February","March","April","May","June","July","August","September","October","November", "December"];
+document.getElementById("datetoday").innerHTML = (days[d.getDay()] + ", " + d.getDate() + " " +month[d.getMonth()]+ " " + d.getFullYear());
