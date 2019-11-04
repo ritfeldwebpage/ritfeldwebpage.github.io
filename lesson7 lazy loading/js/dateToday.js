2ade9b5e5922e0cfa4c9740332657d1c11ec27e0
willemjritfeld <rit16005@byui.edu>
2019-11-04 01:55:07 -0400

    update

--- lesson7 lazy loading/js/dateToday.js
+++
@@ -1,6 +0,0 @@
-var today = new Date();
-
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
-var dateString = today.toLocaleDateString("en-GB", options);
-document.getElementById('currentDate').textContent = dateString;
