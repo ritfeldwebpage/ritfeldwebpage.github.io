c36e9aac3c29281c250c7b81b33c52bcb34b2725
willemjritfeld <rit16005@byui.edu>
2019-11-28 19:11:46 -0400

    update

--- lesson8a/js/lastmodified.js
+++
@@ -1,5 +0,0 @@
-
-var d = new Date();
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var month = ["January","February","March","April","May","June","July","August","September","October","November", "December"];
-document.getElementById("datetoday").innerHTML = (days[d.getDay()] + ", " + d.getDate() + " " +month[d.getMonth()]+ " " + d.getFullYear());
