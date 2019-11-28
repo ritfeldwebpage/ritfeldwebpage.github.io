cd48bd7c0b12e97a1d093e99c31434f78a21b773
willemjritfeld <rit16005@byui.edu>
2019-11-27 20:20:54 -0400

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,10 +1,11 @@
-const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperialAPPID=8f5eeb868c678c79fca553e4efc335f5";
+const apiURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=8f5eeb868c678c79fca553e4efc335f5";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('current-temp')
+    .textContent = jsObject.main.temp;
 
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
