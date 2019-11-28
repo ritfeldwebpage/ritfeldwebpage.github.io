089b1ed3e7d178e6fd35ae2ace3789e68ebbd595
willemjritfeld <rit16005@byui.edu>
2019-11-28 17:12:29 -0400

    update

---
+++ lesson10/js/bannerad.js
@@ -0,0 +1,10 @@
+const thisday = new Date();
+const dayNumber = thisday.getDay();
+
+
+if (dayNumber == 5) {
+    document.getElementById("banner").classList.add("showme");
+}
+else {
+    document.getElementById("banner").classList.add("hideme");
+}
--- lesson10/js/updated.js
+++
@@ -1,8 +0,0 @@
-var lastupd = new Date(document.lastModified);
-numMonth = lastupd.getMonth() + 1;
-numDate = lastupd.getDate();
-numYear = lastupd.getFullYear();
-numHour = lastupd.getHours();
-numMin = lastupd.getMinutes();
-numSec = lastupd.getSeconds();
-document.getElementById('lastUpdate').textContent = (numMonth + "/" + numDate + "/" + numYear + "  " + numHour + ":" + numMin + ":" + numSec);
